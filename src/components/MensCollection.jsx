b0f75812f766c6b73fc31069f241a28d961d5d45
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-07-21 17:14:00 -0400

    Create components for different collections of shoes

---
+++ src/components/MensCollection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MensCollection = () => {
+  return (
+    <div>MensCollection</div>
+  )
+}
+
+export default MensCollection
---
+++ src/components/WomensCollection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WomensCollection = () => {
+  return (
+    <div>WomensCollection</div>
+  )
+}
+
+export default WomensCollection

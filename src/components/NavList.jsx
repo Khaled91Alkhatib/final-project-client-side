cf9d61587f55a190d804dd42fc3e012bf1eb288f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-07-25 20:46:56 -0400

    Implement code to have a responsive modal

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useState, useRef ,useEffect, useContext} from 'react';
 import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 
 import "../styles/NavList.scss";
@@ -6,16 +6,38 @@ import "../styles/NavList.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBagShopping, faPhone } from "@fortawesome/free-solid-svg-icons";
+import ShoppingCart from './ShoppingCart';
 library.add(faBagShopping, faPhone);
 
 const NavList = () => {
+const ref = useRef()
+
   const navigate = useNavigate();
 
   const onClickLogo = () => {
     navigate("/");
   };
 
-  // <button><NavLink to="/products/men"> Shop for Men </NavLink></button>
+  const [cartClick, setCartClick] = useState(false)
+
+  const handleClick = event => {
+    setCartClick(current => !current)
+  }
+
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      // If the modal is open and the clicked target is not within the modal, then close the modal
+      if (cartClick && ref.current && !ref.current.contains(e.target)) {
+        setCartClick(false)
+      }
+    }
+    document.addEventListener("mousedown", checkIfClickedOutside)
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [cartClick])
+
   return (
     <div className="nav-bar">
       <img className='logo' onClick={onClickLogo} src="../logo.png" alt="logo here" />
@@ -24,7 +46,9 @@ const NavList = () => {
       <button className='nav-buttons'><NavLink className="navlink" to="/collection/men"> Men's Collection</NavLink></button>
       <button className='nav-buttons'><NavLink className="navlink" to="/collection/women"> Women's Collection</NavLink></button>
       <button className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-phone" />&nbsp;Contact Us</button>
-      <button className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-bag-shopping" />&nbsp; Shopping Cart</button>
+      <button onClick={handleClick} className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-bag-shopping" />&nbsp; Shopping Cart</button>
+      {cartClick && <ShoppingCart modalRef={ref}/> }
+
       </div>
     </div>
   );

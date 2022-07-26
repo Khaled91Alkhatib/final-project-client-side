1d42dfdc0082dced7183916a144e2d08dee7a8fd
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-07-25 20:51:18 -0400

    Implement Add To Cart code and make it dynamic

--- src/components/SingleProductPage/SingleProduct.jsx
+++ src/components/SingleProductPage/SingleProduct.jsx
@@ -1,40 +1,47 @@
-import React, {useContext, useEffect, useState} from 'react';
-import { NavLink, useParams, useSearchParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useParams, useSearchParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import axios from 'axios';
+import axios from "axios";
 
-import ProductsContext from '../../contexts/ProductsContext';
-import Image from './Image';
-import Colors from './Colors';
-import Sizes from './Sizes';
+import ProductsContext from "../../contexts/ProductsContext";
+import Image from "./Image";
+import Colors from "./Colors";
+import Sizes from "./Sizes";
 
-import './SingleProduct.scss';
+import "./SingleProduct.scss";
+import CartContext from "../../contexts/CartContext";
 
 const SingleProduct = (props) => {
-
   const [id, setId] = useState(Number(useParams().id));
   const [product, setProduct] = useState({});
   const [availableSizes, setAvailableSizes] = useState([]);
   const [images, setImages] = useState([]);
   const [colorsFamily, setColorsFamily] = useState([]);
   const [selectedSize, setSelectedSize] = useState({});
-  
-  const {products} = useContext(ProductsContext);
+  const [counter, setCounter] = useState(1);
+
+  const { products } = useContext(ProductsContext);
+  const { setCart, cart } = useContext(CartContext);
 
   useEffect(() => {
     if (products) {
       getProductById(id);
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
     if (product.sku) {
       // find and set colorsFamily
       // specify which product's color is selected
       const productFamily = product.sku.slice(0, 4);
-      const allColors = products.filter(product => product.sku.slice(0,4) === productFamily).map(row => {
-        return (row.id === product.id) ? {...row, selected: true} : {...row, selected: false}
-      });
+      const allColors = products
+        .filter((product) => product.sku.slice(0, 4) === productFamily)
+        .map((row) => {
+          return row.id === product.id
+            ? { ...row, selected: true }
+            : { ...row, selected: false };
+        });
       setColorsFamily(allColors);
 
       // make images array ready
@@ -43,35 +50,37 @@ const SingleProduct = (props) => {
       if (product.image2) imagesArray.push(product.image2);
       if (product.image3) imagesArray.push(product.image3);
       setImages(imagesArray);
-      
+
       // update the address bar when product has been changed
-      window.history.replaceState('', '',`/collection/${product.category}/${id}`);
+      window.history.replaceState(
+        "",
+        "",
+        `/collection/${product.category}/${id}`
+      );
 
       // reset selected size when product has been changed
       setSelectedSize({});
     }
-  },[products, product, id]);
-
+  }, [products, product, id]);
 
   const getProductById = (id) => {
-    axios.get(`http://localhost:8100/api/products/${id}`)
-    .then((response) => {
+    axios.get(`http://localhost:8100/api/products/${id}`).then((response) => {
       // handle success
-      setProduct(prev => response.data.product);
-      setAvailableSizes(prev => (response.data.availableSizes));
-    }) 
+      setProduct((prev) => response.data.product);
+      setAvailableSizes((prev) => response.data.availableSizes);
+    });
   };
 
   // image click left
   const rotateLeft = () => {
     const singleImage = images.shift();
-    setImages(prev => [...prev, singleImage] );
+    setImages((prev) => [...prev, singleImage]);
   };
 
   // image click right
   const rotateRight = () => {
     const singleImag = images.pop();
-    setImages(prev => [singleImag, ...prev] );
+    setImages((prev) => [singleImag, ...prev]);
   };
 
   const changeColorHandler = (id) => {
@@ -80,35 +89,75 @@ const SingleProduct = (props) => {
   };
 
   const onSelectSize = (data) => {
-    setSelectedSize(prev => (data));
+    setSelectedSize((prev) => data);
   };
 
-  const onAdd = () => {
-    if (selectedSize.id)
-      console.log('ADD');
-      // addToCart([{barcode: selectedSize.barcode, id: selectedSize.id, qty: 1, name: product.name, color: product.color, price: product.price, img: product.image1, size: selectedSize.size}])
-  }
+  const onPlusClick = () => {
+    setCounter(counter + 1);
+  };
+  const onMinusClick = () => {
+    if (counter > 1) {
+      setCounter(counter - 1);
+    }
+  };
 
-  // console.log('👟',product);           // 🚨🚨🚨
+
+  const addToCart = () => {
+    // console.log("ADD");
+    product.size = selectedSize.size;
+    product.quantity = counter;
+    const existingObjectInCart = cart.find((item) => {
+      return (item.sku === product.sku && selectedSize.size === item.size)
+    })
+    if (!existingObjectInCart){
+      setCart([...cart, product]);
+    } else {
+      const foundItemInCart = cart.findIndex((item) => {
+        return item.sku === existingObjectInCart.sku && item.size === existingObjectInCart.size
+      })
+      const updatedItem = {...cart[foundItemInCart], quantity: cart[foundItemInCart].quantity += counter}
+      const updatedCart = [...cart]
+      updatedCart[foundItemInCart] = updatedItem
+      setCart(updatedCart)
+
+      console.log('found', foundItemInCart);
+      console.log("exists", existingObjectInCart)
+    }
+  };
+
+  // console.log("counter", counter)
+  console.log("👟", product); // 🚨🚨🚨
   // console.log('⚫️⚪️',colorsFamily);    // 🚨🚨🚨
   // console.log('🗾',images);            // 🚨🚨🚨
   // console.log('◻️◾️',availableSizes);   // 🚨🚨🚨
-  // console.log('💢',selectedSize);     // 🚨🚨🚨
+  console.log("💢", selectedSize); // 🚨🚨🚨
 
   return (
-    <div className='single-product'>
-      <div className='single-box'>
-        <Image images={images} onLeft={rotateLeft} onRight={rotateRight}/>
-        <div className='item-details'>
+    <div className="single-product">
+      <div className="single-box">
+        <Image images={images} onLeft={rotateLeft} onRight={rotateRight} />
+        <div className="item-details">
           <h2>SKU #{product.sku} </h2>
           <span>{product.name}</span>
           <br />
           <span>${(product.price / 100).toFixed(2)}</span>
           <br />
-          <Sizes availableSizes={availableSizes} onSelectSize={onSelectSize} select={selectedSize} onAdd={onAdd}/>
+          <Sizes
+            availableSizes={availableSizes}
+            onSelectSize={onSelectSize}
+            select={selectedSize} /*onAdd={onAdd}*/
+          />
+          <div className="plus-counter-minus">
+            <button className="plus-minus" onClick={onMinusClick}><FontAwesomeIcon icon="fa-solid fa-circle-minus" /></button>
+            <div>{counter}</div>
+            <button className="plus-minus" onClick={onPlusClick}><FontAwesomeIcon icon="fa-solid fa-circle-plus" /></button>
+          </div>
+          <button disabled={!selectedSize.id} onClick={addToCart}>
+            Add To Cart
+          </button>
           <br />
           <span>color : {product.color}</span>
-          <Colors colorsFamily={colorsFamily} onColor={changeColorHandler}/>
+          <Colors colorsFamily={colorsFamily} onColor={changeColorHandler} />
           <span>{product.description}</span>
         </div>
       </div>

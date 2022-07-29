a9058b113643dd053b6b0fdc9600dd4bd042bba2
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-07-28 22:36:54 -0700

    different navbar for admin

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -49,18 +49,32 @@ const NavList = (props) => {
 
   return (
     <div className="nav-bar">
-      <div className="all-buttons">
-        <div className='left-and-right-navs'>
-          <button className='nav-buttons'><NavLink className="navlink" to="/collection/men"> Men's Collection</NavLink></button>
-          <button className='nav-buttons'><NavLink className="navlink" to="/collection/women"> Women's Collection</NavLink></button>
+      {!props.user.name &&
+        <div className="all-buttons">
+          <div className='left-and-right-navs'>
+            <button className='nav-buttons'><NavLink className="navlink" to="/collection/men"> Men's Collection</NavLink></button>
+            <button className='nav-buttons'><NavLink className="navlink" to="/collection/women"> Women's Collection</NavLink></button>
+          </div>
+          <img className='logo' onClick={onClickLogo} src="../logo.png" alt="logo here" />
+          <div className='left-and-right-navs'>
+            <button className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-phone" size='lg' />&nbsp; Contact Us</button>
+            <button onClick={() => { setCartClick(true); }} className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-bag-shopping" size='lg' /><span className='badge badge-warning' id='lblCartCount'> {numberOfItemsInCart} </span>&nbsp; Shopping Cart</button>
+            {cartClick && <ShoppingCart continueShopping={setCartClick} modalRef={ref} />}
+          </div>
         </div>
-        <img className='logo' onClick={onClickLogo} src="../logo.png" alt="logo here" />
-        <div className='left-and-right-navs'>
-          <button className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-phone" size='lg' />&nbsp; Contact Us</button>
-          <button onClick={() => { setCartClick(true); }} className='nav-buttons'><FontAwesomeIcon icon="fa-solid fa-bag-shopping" size='lg' /><span className='badge badge-warning' id='lblCartCount'> {numberOfItemsInCart} </span>&nbsp; Shopping Cart</button>
-          {cartClick && <ShoppingCart continueShopping={setCartClick} modalRef={ref} />}
+      }
+      {props.user.name &&
+
+        <div>
+          <img className='logo' src="../logo.png" alt="logo" />
+          <button 
+            className='nav-buttons'
+            onClick={() => setUser({})}
+          >
+            Logout
+          </button>
         </div>
-      </div>
+      }
     </div>
   );
 };
